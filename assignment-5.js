e24d69d23b69ee208520f12f571608da6c08e717
mdsumonsr143 <mdsumon37529@gmail.com>
2025-08-29 18:26:04 +0600

    added the js file

---
+++ assignment-5.js
@@ -0,0 +1,13 @@
+// Initialize counts
+    let heartCount = 0;
+    let coinCount = 100;
+    let copyCount = 0;
+
+    // DOM elements
+    const heartDisplay = document.getElementById('heartCount');
+    const coinDisplay = document.getElementById('coinCount');
+    const copyDisplay = document.getElementById('copyCount');
+    const callHistoryDiv = document.querySelector('.call-history');
+
+    // Heart click
+    
